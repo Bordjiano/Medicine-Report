a90e50a0aa47c54898bf57e1993180a48cbc65fc
Bordjiano <vanya.misyura@gmail.com>
2023-05-31 09:09:56 +0300

    Start project

---
+++ src/test/java/com/example/medicinereport/MedicineReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicinereport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
